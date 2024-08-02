9ec1270a3279de0054d1722444ea33517563f714
Neo40 <tron36.cc@gmail.com>
2024-08-01 22:31:37 -0300

    Adding An Example of the usage for array

---
+++ LanguageBasics/ArrayDemo.java
@@ -0,0 +1,44 @@
+package LanguageBasics;
+//An array is a container object that holds a fixed number of values of a single type.
+// The length of an array is established when the array is created. After creation, its length is fixed.
+public class ArrayDemo {
+    public static void main(String[] args) {
+        // declares an array of integers
+        int[] anArray;
+        // allocates memory for 10 integers
+        anArray = new int[10];
+        // initialize first element
+        anArray[0] = 100;
+        // initialize second element
+        // and so forth
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+        System.out.println("Element at index 0: "
+                + anArray[0]);
+        System.out.println("Element at index 1: "
+                + anArray[1]);
+        System.out.println("Element at index 2: "
+                + anArray[2]);
+        System.out.println("Element at index 3: "
+                + anArray[3]);
+        System.out.println("Element at index 4: "
+                + anArray[4]);
+        System.out.println("Element at index 5: "
+                + anArray[5]);
+        System.out.println("Element at index 6: "
+                + anArray[6]);
+        System.out.println("Element at index 7: "
+                + anArray[7]);
+        System.out.println("Element at index 8: "
+                + anArray[8]);
+        System.out.println("Element at index 9: "
+                + anArray[9]);
+    }
+}
+
