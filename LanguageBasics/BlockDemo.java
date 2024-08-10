5f2758bbb4bf781383a0ab8fc5d1798841b0a14f
Neo40 <tron36.cc@gmail.com>
2024-08-09 22:53:23 -0300

    Exemple of the use of blocks

---
+++ LanguageBasics/BlockDemo.java
@@ -0,0 +1,16 @@
+package LanguageBasics;
+
+//A block is a group of zero or more statements between balanced braces and can be used anywhere
+// a single statement is allowed.
+// The following example, BlockDemo, illustrates the use of blocks:
+public class BlockDemo {
+    public static void main(String[] args) {
+        boolean condition = true;
+        if (condition) { // begin block 1
+            System.out.println("Condition is true.");
+        } // end block one
+        else { // begin block 2
+            System.out.println("Condition is false.");
+        } // end block 2
+    }
+}
